4962dbbed009f5bbf4829ba855511221294e70ea
Jhon Navarro <jhonz.alex.214163@gmail.com>
2026-05-01 03:06:55 -0500

    Agregar calculo de bateria

--- CalculoBateria.php
+++ CalculoBateria.php
@@ -1,8 +1,17 @@
 <?php
+echo "Ingrese bateria 1: ";
 $b1 = floatval(trim(fgets(STDIN)));
+
+echo "Ingrese bateria 2: ";
 $b2 = floatval(trim(fgets(STDIN)));
+
+echo "Ingrese bateria 3: ";
 $b3 = floatval(trim(fgets(STDIN)));
+
+echo "Ingrese bateria 4: ";
 $b4 = floatval(trim(fgets(STDIN)));
+
+echo "Ingrese bateria 5: ";
 $b5 = floatval(trim(fgets(STDIN)));
 
 $promedio = ($b1 + $b2 + $b3 + $b4 + $b5) / 5;
